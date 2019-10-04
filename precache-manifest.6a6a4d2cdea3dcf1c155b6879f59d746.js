db52f6633d4bf9fe159aab9d0db9ba99d1a970c9
vignesh222s <iamvigneshsundaram@gmail.com>
2019-10-04 18:31:27 +0530

    Updates

---
+++ precache-manifest.6a6a4d2cdea3dcf1c155b6879f59d746.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d323992907acf77ebaacd24e85b3085",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "9c98e986d7d5f5ac2541",
+    "url": "/react-app/static/css/main.0989f10c.chunk.css"
+  },
+  {
+    "revision": "0cca02fe10f4d25a3546",
+    "url": "/react-app/static/js/2.e74b291d.chunk.js"
+  },
+  {
+    "revision": "9c98e986d7d5f5ac2541",
+    "url": "/react-app/static/js/main.fed08e57.chunk.js"
+  },
+  {
+    "revision": "7a48a05a83540a28e3e1",
+    "url": "/react-app/static/js/runtime-main.c94c6395.js"
+  }
+]);
